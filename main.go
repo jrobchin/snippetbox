f53f76ff5d82b8f2a01a3840090eb3a5f1328b14
Jason Chin <jason.rob.chin@gmail.com>
2020-07-17 11:04:03 -0400

    basic routing

--- main.go
+++ main.go
@@ -7,13 +7,30 @@ import (
 
 // Home handler that writes a hello message
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Returns a specified snipped
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a snippet"))
+}
+
+// Creates a snippet
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("create a snippet"))
 }
 
 func main() {
 	// Initialize a servemux to route handlers
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	// Start new web server on port 4000
 	log.Println("Starting server on :4000")
