f405840e752e9eedd4e38a9c6edd9165f30281f7
Jason Chin <jason.rob.chin@gmail.com>
2020-07-18 09:57:34 -0400

    logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,11 +4,22 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Log errors to a file
+	f, err := os.OpenFile("error.log", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		infoLog.Fatal(err)
+	}
+	defer f.Close()
+	errorLog := log.New(f, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	// Initialize a servemux to route handlers
 	mux := http.NewServeMux()
@@ -19,8 +30,14 @@ func main() {
 	fileServer := http.FileServer(http.Dir("../../ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	server := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
 	// Start new web server on port 4000
-	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	infoLog.Printf("Starting server on %s", *addr)
+	err = server.ListenAndServe()
+	errorLog.Fatal(err)
 }
