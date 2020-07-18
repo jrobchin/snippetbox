67153338e21bf7ec2d72ca9da68f13f686c01585
Jason Chin <jason.rob.chin@gmail.com>
2020-07-18 10:13:47 -0400

    basic dependency injection

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"path/filepath"
 	"strconv"
 	"text/template"
 )
 
-func renderTemplate(w http.ResponseWriter, templatePath string) error {
+func (app *application) renderTemplate(w http.ResponseWriter, templatePath string) error {
 	files := []string{
 		filepath.Join("../../ui/html", templatePath),
 		filepath.Join("../../ui/html/footer.partial.tmpl"),
@@ -18,14 +17,14 @@ func renderTemplate(w http.ResponseWriter, templatePath string) error {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return err
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
 
@@ -33,17 +32,18 @@ func renderTemplate(w http.ResponseWriter, templatePath string) error {
 }
 
 // Home handler that writes a hello message
-func home(w http.ResponseWriter, r *http.Request) {
+// Define methods on the application struct to gain access to dependencies
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
 
-	renderTemplate(w, "home.page.tmpl")
+	app.renderTemplate(w, "home.page.tmpl")
 }
 
 // Returns a specified snipped
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -54,7 +54,7 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 // Creates a snippet
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	// Only allow POST requests
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,25 +7,31 @@ import (
 	"os"
 )
 
+// Application-wide dependencies
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 
-	// Log errors to a file
-	f, err := os.OpenFile("error.log", os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		infoLog.Fatal(err)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Initialize application
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
 	}
-	defer f.Close()
-	errorLog := log.New(f, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	// Initialize a servemux to route handlers
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	fileServer := http.FileServer(http.Dir("../../ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
@@ -38,6 +44,6 @@ func main() {
 
 	// Start new web server on port 4000
 	infoLog.Printf("Starting server on %s", *addr)
-	err = server.ListenAndServe()
+	err := server.ListenAndServe()
 	errorLog.Fatal(err)
 }
